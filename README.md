dc002b2eddfe6b03ec36f1fc68f639bcd9357dc9
Meng Li <34143965+Meng6@users.noreply.github.com>
2024-04-27 20:16:28 +0000

    Add docs and scripts for baselines and two roles

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# text-clustering-interpreter
+# Large Language Models are Interpreters of Text Clustering Results
+This is a course project for IS 557 Applied Machine Learning: Team Project.
+
+The [dataset](data/mtop.csv) used for this project is based on the ClusterLLM (Zhang et al., 2023), which preprocessed the MTOP dataset (Li et al., 2021) by removing intents with only a few instances and keeping English-only data. Only the music domain has been selected for further experimentation which consists of 1341 samples and 24 intentions.
+
+The `src` folder contains four *Python* scripts:
+
+- [1_baselines.py](src/1_baselines.py)
+
+- [2_1_musician_ds_one_model.py](src/2_1_musician_ds_one_model.py)
+
+- [2_2_musician_ds_multi_models.py](src/2_2_musician_ds_multi_models.py)
+
+- [3_llama3_mistral.py](src/3_llama3_mistral.py)
+
+**Environment setup**
+
+1. Download [Ollama](https://ollama.com/download) and install pre-trained LLMs: 
+```
+# go to the command line and run the following commands
+# to pull pre-trained LLMs to your local machine
+ollama run llama3
+ollama run mistral
+``` 
+
+2. Install [brew](https://brew.sh/)
+
+3. Install *miniconda* (restart your terminal afterwards)
+```
+brew install --cask miniconda
+conda init zsh # (or conda init bash)
+```
+
+3. Setup *Python* virtual environment
+```
+conda env create -f environment.yml -n is577
+conda activate is577
+```
+
+4. Run scripts
+```
+# Pick the script
+python src/1_baselines.py
+```
+
+References
+
+Yuwei Zhang, Zihan Wang, and Jingbo Shang. 2023. ClusterLLM: Large Language Models as a Guide for Text Clustering. In *Proceedings of the 2023 Conference on Empirical Methods in Natural Language Processing*, pages 13903–13920, Singapore. Association for Computational Linguistics.
+
+Haoran Li, Abhinav Arora, Shuohui Chen, Anchit Gupta, Sonal Gupta, and Yashar Mehdad. 2021. MTOP: A comprehensive multilingual task-oriented semantic parsing benchmark. In *Proceedings of the 16th Conference of the European Chapter of the Association for Computational Linguistics: Main Volume*, pages 2950–2962, Online. Association for Computational Linguistics.
---
+++ src/2_1_musician_ds_one_model.py
@@ -0,0 +1,89 @@
+import ollama, random
+import pandas as pd
+from InstructorEmbedding import INSTRUCTOR
+from sklearn.metrics.pairwise import cosine_similarity
+from func_timeout import func_timeout, FunctionTimedOut
+
+############################################################
+LLM_MODEL = 'llama3' # 'llama3' OR 'mistral'
+NUM_SAMPLES_PER_CLUSTER = 1 # pick one from {1, 3, 6, 9}
+NUM_CLUSTERS = 24
+REPETITIONS = 50
+############################################################
+
+def get_llms_response(dataset, num_clusters, num_samples_per_cluster, llm_model):
+    cid, prompt, true = 1, "Below is the clustering results of user intentions in music domain:\n\n", []
+    for cluster in random.sample(intents, num_clusters):
+        true.append(cluster)
+        curr_intent = data[data['intent_label'] == cluster]
+        samples = curr_intent.sample(min(num_samples_per_cluster, len(curr_intent)))
+        prompt = prompt + "Cluster {cid}: {samples}\n".format(cid=cid, samples=samples['input'].tolist())
+        cid += 1
+    prompt = prompt + """
+There are two experts who interpret these clusters differently, one is a musician, the other is a data scientist.
+Please mimic their conversation and try to reach agreement at the end. Only return the agreement without explanation in the format of "cluster number: interpretation of the cluster". Use one or two words to interpret each cluster.
+"""
+
+    response = ollama.chat(model=llm_model, messages=[
+        {'role': 'user', 'content': prompt},
+        {'role': 'user', 'content': 'Extract each cluster label in order after reaching the agreement. Please provide your response in the following format directly: "interpretation 1\ninterpretation 2\n...". No extra sentences are included. The following responses should start from cluster 1 and end with cluster 24. Here are the interpreted clusters separated by a new line (do not repeat or paraphrase this sentence):\n'}
+        ])['message']['content']
+    
+    return (response, true)
+
+def parse_response(response):
+    pred_ = [x.strip() for x in response.split('\n')]
+    pred = [item for item in pred_ if item != '']
+    for x in pred:
+        if x == '':
+            pred.remove(x)
+    return pred
+
+model = INSTRUCTOR('hkunlp/instructor-large')
+ordered_true = ['remove from playlist music', 'repeat all music', 'skip track music', 'repeat all off music', 'rewind music', 'play media', 'loop music', 'like music', 'previous track music', 'add to playlist music', 'play music', 'fast forward music', 'stop music', 'question music', 'pause music', 'resume music', 'dislike music', 'get track info music', 'create playlist music', 'get lyrics music', 'delete playlist music', 'start shuffle music', 'set default provider music', 'replay music']
+
+count, res = 0, pd.DataFrame()
+data = pd.read_csv("data/mtop.csv")
+data = data[data['domain_label'] == 'music'] # 24 intentions
+intents = data['intent_label'].unique().tolist()
+
+while count < REPETITIONS:
+
+    print("Iteration: ", count)
+
+    try:
+        (response, true) = func_timeout(90, get_llms_response, args=(intents, NUM_CLUSTERS, NUM_SAMPLES_PER_CLUSTER, LLM_MODEL))
+    except FunctionTimedOut:
+        continue
+    pred = parse_response(response)
+
+    if len(pred) == 24:
+        instruction = "Represent the Intent sentence: "
+        # reorder pred based on ordered_true
+        try:
+            ordered_pred = []
+            for true_label in ordered_true:
+                idx = true.index(true_label)
+                ordered_pred.append(pred[idx])
+            count = count + 1
+
+            sentences_pred = [[instruction, x] for x in ordered_pred]
+            sentences_true = [[instruction, x] for x in ordered_true]
+            embeddings_pred = model.encode(sentences_pred)
+            embeddings_true = model.encode(sentences_true)
+            similarities = cosine_similarity(embeddings_pred,embeddings_true)
+
+            if count == 1:
+                rows = []
+                for i in range(24):
+                    rows.append([str(i+1), ordered_true[i], similarities[i][i]])
+                res = pd.DataFrame(columns=["cluster", "true", count], data=rows)
+            else:
+                res[count] = similarities.diagonal()
+        except:
+            print("Skip this iteration.")
+
+next_row_idx = len(res)
+res.loc[next_row_idx, ["cluster"]+[x+1 for x in range(count)]] = ["avg"] + [res[x+1].mean() for x in range(count)]
+res.loc[next_row_idx, "true"] = res.loc[next_row_idx, [x+1 for x in range(count)]].mean()
+res.to_csv("output/musician_ds_onemodel_{llm}_metrics_{x}_{y}.csv".format(llm=LLM_MODEL, x=NUM_CLUSTERS, y=NUM_SAMPLES_PER_CLUSTER), index=False)
